33996b6d483ce348dd098894e2714fa89b1281b2
Muhammad Usman Sajjad <usman.sajjad@systemsltd.com>
2017-08-30 03:56:27 -0700

    Add project files.

---
+++ TestDeployment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.Tutorials;
+
+namespace TestDeployment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deployment dep = new Deployment();
+            dep.ShouldBeAbleToDeployAContract().Wait();
+        }
+    }
+}
